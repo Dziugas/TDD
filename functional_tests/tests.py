10d09b6842f609871067b23884878d3ade8319d2
Dziugas <dziugas.tornau@gmail.com>
2019-05-01 16:37:23 +0300

    added deploy tools for setting up nginx and gunicorn automatically

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -34,13 +34,10 @@ class NewVisitorTest(StaticLiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-
-
     def test_can_start_a_list_for_one_user(self):
         #Edith has heard about a cool new online to-do app. 
         #She goes to check out its homepage.
         self.browser.get(self.live_server_url)
-
 
         #She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
